360a048b88794793bfd3a42277ec7dc77215b7cd
JEunseo <jjeseo1010@gmail.com>
2024-09-12 12:26:06 +0900

    파일변경

--- backend/Hotel-Spring/src/main/java/com/joeunseo/hotel/model/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.joeunseo.hotel.model.repository;
-
-import com.joeunseo.hotel.model.entity.PlayerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
-}
-*/
--- backend/Hotel-Spring/src/main/java/com/joeunseo/hotel/util/PageProxy.java
+++
@@ -1,5 +0,0 @@
-package com.joeunseo.hotel.util;
-
-public class PageProxy {
-
-}
